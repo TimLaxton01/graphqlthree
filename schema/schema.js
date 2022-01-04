c0b4da2aab4bbfbfbf17c30d005210ec09512e58
TimLaxton01 <tlaxton@twigeducatio.com>
2022-01-04 17:51:26 +0000

    added GraphQLID

--- schema/schema.js
+++ schema/schema.js
@@ -1,37 +1,37 @@
-const graphql = require('graphql') ;
-const _ = require('lodash')
+const graphql = require("graphql");
+const _ = require("lodash");
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 const movies = [
-    {name: 'Joker', genre: 'Drama', id: '1'},
-    {name: 'Moonrise Kingdom', genre: 'Romance', id: '2'},
-    {name: 'La La Land', genre: 'Musical', id: '3'},
-    {name: 'Interstellar', genre: 'Sci-Fi', id: '4'},
-]
+  { name: "Joker", genre: "Drama", id: "1" },
+  { name: "Moonrise Kingdom", genre: "Romance", id: "2" },
+  { name: "La La Land", genre: "Musical", id: "3" },
+  { name: "Interstellar", genre: "Sci-Fi", id: "4" },
+];
 
 const MovieType = new GraphQLObjectType({
-    name: 'Movie',
-    fields: () => ({
-        id: {type: GraphQLString},
-        name: {type: GraphQLString },
-        genre: {type: GraphQLString }
-    })
-})
+  name: "Movie",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: () => ({
-        movie: {
-            type: MovieType,
-            args: { id: {type: GraphQLString }},
-            resolve(parent, args){
-             return   _.find(movies, {id: args.id})
-            }
-        }
-    })
-})
+  name: "RootQueryType",
+  fields: () => ({
+    movie: {
+      type: MovieType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(movies, { id: args.id });
+      },
+    },
+  }),
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+});
