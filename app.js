9b7c7f76a500d595ec02d6021013d827158b110c
TimLaxton01 <tlaxton@twigeducatio.com>
2022-01-04 14:59:11 +0000

    added schema

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    
+}))
 
 app.listen(8000, () => {
     console.log('Server is running on Port 8000')
