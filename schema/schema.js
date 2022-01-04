99acedf5d12ea186ad7ca50c32226fc940da81da
TimLaxton01 <tlaxton@twigeducatio.com>
2022-01-04 15:07:40 +0000

    added root schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,29 @@
 const graphql = require('graphql') 
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
-    fields: () =>({
+    fields: () => ({
         id: {type: GraphQLString},
         name: {type: GraphQLString },
         genre: {type: GraphQLString }
     })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: () => ({
+        movie: {
+            type: MovieType,
+            args: { id: {type: GraphQLString }},
+            resolve(parent, args){
+
+            }
+        }
+    })
+})
+
+module.exports = newGraphQLSchema({
+    query: RootQuery
 })
